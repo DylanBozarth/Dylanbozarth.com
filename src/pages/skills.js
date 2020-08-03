040193352cb3b8da8c8ace440d0178b1aa767fd8
Dylan <dylanabozarth@gmail.com>
2020-08-03 10:15:12 -0700

    added sql

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -33,7 +33,12 @@ export const Skills = () => (
 
             <div className="profile-bio">
               <p>HTML</p>
-              <p>Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.</p>
+              <p>
+                Hypertext Markup Language (HTML) is the standard markup language
+                for documents designed to be displayed in a web browser. It can
+                be assisted by technologies such as Cascading Style Sheets (CSS)
+                and scripting languages such as JavaScript.
+              </p>
             </div>
           </aside>{" "}
         </div>
@@ -43,7 +48,12 @@ export const Skills = () => (
 
             <div className="profile-bio">
               <p>CSS</p>
-              <p>Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.</p>
+              <p>
+                Cascading Style Sheets (CSS) is a style sheet language used for
+                describing the presentation of a document written in a markup
+                language like HTML. CSS is a cornerstone technology of the World
+                Wide Web, alongside HTML and JavaScript.
+              </p>
             </div>
           </aside>{" "}
         </div>
@@ -53,7 +63,14 @@ export const Skills = () => (
 
             <div className="profile-bio">
               <p>Javascript</p>
-              <p>Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web. JavaScript enables interactive web pages and is an essential part of web applications. The vast majority of websites use it for client-side page behavior, and all major web browsers have a dedicated JavaScript engine to execute it.</p>
+              <p>
+                Alongside HTML and CSS, JavaScript is one of the core
+                technologies of the World Wide Web. JavaScript enables
+                interactive web pages and is an essential part of web
+                applications. The vast majority of websites use it for
+                client-side page behavior, and all major web browsers have a
+                dedicated JavaScript engine to execute it.
+              </p>
             </div>
           </aside>{" "}
         </div>
@@ -63,10 +80,14 @@ export const Skills = () => (
 
             <div className="profile-bio">
               <p>React</p>
-              <p>React (also known as React.js or ReactJS) is an open-source JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies.</p>
+              <p>
+                React (also known as React.js or ReactJS) is an open-source
+                JavaScript library for building user interfaces. It is
+                maintained by Facebook and a community of individual developers
+                and companies.
+              </p>
             </div>
           </aside>{" "}
-          
         </div>
       </div>
       <div className="row">
@@ -76,7 +97,12 @@ export const Skills = () => (
 
             <div className="profile-bio">
               <p>React-native</p>
-              <p>React Native is an open-source mobile application framework created by Facebook. It is used to develop applications for Android, iOS, Web and UWP by enabling developers to use React along with native platform capabilities.</p>
+              <p>
+                React Native is an open-source mobile application framework
+                created by Facebook. It is used to develop applications for
+                Android, iOS, Web and UWP by enabling developers to use React
+                along with native platform capabilities.
+              </p>
             </div>
           </aside>{" "}
         </div>
@@ -86,10 +112,16 @@ export const Skills = () => (
 
             <div className="profile-bio">
               <p>Node.Js</p>
-              <p>Node.js is an open-source, cross-platform, JavaScript runtime environment that executes JavaScript code outside a web browser. Node.js lets developers use JavaScript to write command line tools and for server-side scripting—running scripts server-side to produce dynamic web page content before the page is sent to the user's web browser.</p>
+              <p>
+                Node.js is an open-source, cross-platform, JavaScript runtime
+                environment that executes JavaScript code outside a web browser.
+                Node.js lets developers use JavaScript to write command line
+                tools and for server-side scripting—running scripts server-side
+                to produce dynamic web page content before the page is sent to
+                the user's web browser.
+              </p>
             </div>
           </aside>{" "}
-          
         </div>
         <div className="col-sm ">
           <aside className="profile-card asidescss">
@@ -97,10 +129,12 @@ export const Skills = () => (
 
             <div className="profile-bio">
               <p>SCSS</p>
-              <p>Sass (short for syntactically awesome style sheets) is a style sheet language. Used for advanced styling of web applications. </p>
+              <p>
+                Sass (short for syntactically awesome style sheets) is a style
+                sheet language. Used for advanced styling of web applications.{" "}
+              </p>
             </div>
           </aside>{" "}
-          
         </div>
         <div className="col-sm ">
           <aside className="profile-card asideredux">
@@ -108,38 +142,74 @@ export const Skills = () => (
 
             <div className="profile-bio">
               <p>Redux</p>
-              <p>Redux is an open-source JavaScript library for managing application state. It is most commonly used with libraries such as React or Angular for building user interfaces. Similar to (and inspired by) Facebook's Flux architecture</p>
+              <p>
+                Redux is an open-source JavaScript library for managing
+                application state. It is most commonly used with libraries such
+                as React or Angular for building user interfaces. Similar to
+                (and inspired by) Facebook's Flux architecture
+              </p>
             </div>
           </aside>{" "}
-          
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm ">
+          <aside className="profile-card asidessql">
+            <header></header>
+
+            <div className="profile-bio">
+              <p>SQL</p>
+              <p>
+                Structured Query Language is a domain-specific
+                language used in programming and designed for managing data held
+                in a relational database management system
+              </p>
+            </div>
+          </aside>{" "}
         </div>
       </div>
       <br />
       <h1 className="skillevidence">Relevant links</h1>
       <div className="row linkboxes">
         <div className="col-sm evidencelink">
-        <a className="evidencelink" target="_blank"
-        rel="noopener noreferrer" href="https://github.com/Imstupidpleasehelp">
-        <div className="col-md-4">Github
-          <div className="skillboximggithub"></div> <br />
-          
-        </div></a></div>
+          <a
+            className="evidencelink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Imstupidpleasehelp"
+          >
+            <div className="col-md-4">
+              Github
+              <div className="skillboximggithub"></div> <br />
+            </div>
+          </a>
+        </div>
         <div className="col-sm evidencelink">
-        <a className="evidencelink" target="_blank"
-        rel="noopener noreferrer" href="https://codepen.io/dashboard/?cursor=ZD0wJm89MSZwPTEmdj00MTU1NTkyMg==">
-        <div className="col-md-4">Codepen
-          <div className="skillboximgcodepen"></div> <br />
-          
-        </div></a></div>
+          <a
+            className="evidencelink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://codepen.io/dashboard/?cursor=ZD0wJm89MSZwPTEmdj00MTU1NTkyMg=="
+          >
+            <div className="col-md-4">
+              Codepen
+              <div className="skillboximgcodepen"></div> <br />
+            </div>
+          </a>
+        </div>
         <div className="col-sm evidencelink">
-        <a className="evidencelink" target="_blank"
-        rel="noopener noreferrer" href="https://www.freecodecamp.org/dylanbozarth">
-        <div className="col-md-4">Freecodecamp
-          {" "}
-          <div className="skillboximgfcc"></div>
-          <br />
-          
-        </div></a></div>
+          <a
+            className="evidencelink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.freecodecamp.org/dylanbozarth"
+          >
+            <div className="col-md-4">
+              Freecodecamp <div className="skillboximgfcc"></div>
+              <br />
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   </motion.div>
