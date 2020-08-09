d8894d87b2cc2cbc01dd48294099343369874869
Dylan <dylanabozarth@gmail.com>
2020-08-08 21:20:22 -0700

    fixed typo

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,15 +1,14 @@
 import React from "react";
-import {gsap, TweenMax, Power3} from 'gsap'
 
 export const Homepage = () => (
   
     
 <div id="container" className="homepagebox">
-  Looking for a
+ <p className="fliptext"> Looking for </p>
   <div id="flip">
-    <div><div>A website?</div></div>
-    <div><div>An App?</div></div>
-    <div><div>A web developer?</div></div>
+    <div><div> <p className="fliptext">A website?</p></div></div>
+    <div><div> <p className="fliptext">An App?</p></div></div>
+    <div><div> <p className="fliptext">A web developer?</p></div></div>
   </div>
   
 </div>
