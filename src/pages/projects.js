457a8eb1ea66c5a6d7cdd534a1379f052b2f42ff
Dylan <dylanabozarth@gmail.com>
2020-08-13 11:49:50 -0700

    added icons and fonts

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -22,69 +22,87 @@ export const Projects = () => (
     variants={changepage}
     transition={pagetransition}
   >
-    <div className="grid-container"><h1 className="projectstitle">Commercial Web projects</h1>
-       <div className="row">
-         
-       <div className="griditem">
-      <a
-        href="https://skylimfitness.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      ><h2 className="projectlink">SkyLimit Fitness</h2>
-        <div className="flip">
-          <div className="front skylimit">
-            <h1 className="text-shadow skylimittitle"></h1>
-          </div>
-          <div className="back">
-            
-            <p>
-              SkyLimit Fitness, a personal trainer's website. With e-commerce
-              and blog functionality. Made with React and SCSS
-            </p>
-          </div>
+    <div className="grid-container">
+      <h1 className="projectstitle">Commercial Web projects</h1>
+      <div className="row">
+        <div className="griditem">
+          <a
+            href="https://skylimfitness.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className="projectlink skylimittext">SkyLimit Fitness</h2>
+            <div className="flip">
+              <div className="front skylimit">
+                <h1 className="text-shadow skylimittitle"></h1>
+              </div>
+              <div className="back">
+                <p>
+                  SkyLimit Fitness, a personal trainer's website. With
+                  e-commerce and blog functionality. Made with React and SCSS
+                </p>
+                <div className="row iconrow">
+                  <div className="reacticon"></div>
+                  
+                  <div className="scssicon"></div>
+                  <div className="paypalicon"></div>
+                </div>
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-</div>
-<div className="griditem">
-      <a
-        href="https://76y7e.csb.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      ><h2 className="projectlink">Spokane Power Stroke</h2>
-        <div className="flip">
-          <div className="front hws">
-            <h1 className="text-shadow skylimittitle"></h1>
-          </div>
-          <div className="back">
-           
-            <p>
-             History's Armory, a website for collectors and enthusiasts to buy historical reproduction armor and weapons. Made with React and Scss. 
-            </p>
-          </div>
+        <div className="griditem">
+          <a
+            href="https://76y7e.csb.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className="projectlink powerstroketext">
+              Spokane Power Stroke
+            </h2>
+            <div className="flip">
+              <div className="front powerstroke">
+                <h1 className="text-shadow skylimittitle"></h1>
+              </div>
+              <div className="back">
+                <p> lorem asdnasdnasndasdlaksdjlaksdjo</p>
+                <div className="row iconrow">
+                  <div className="reacticon"></div>
+                  <div className="cssicon"></div>
+                  <div className="scssicon"></div>
+                </div>
+              </div>
+              
+            </div>
+          </a>
         </div>
-      </a>
-</div>
-<div className="griditem">
-      <a
-        href="https://srv9z.codesandbox.io/"
-        target="_blank"
-        rel="noopener noreferrer"
-      ><h2 className="projectlink">History's Armory</h2>
-        <div className="flip">
-          <div className="front munchapp">
-            <h1 className="text-shadow skylimittitle"></h1>
-          </div>
-          <div className="back">
-            
-            <p>
-              A food ordering app, made with React. With firebase and google log-in functionality. 
-            </p>
-          </div>
+        <div className="griditem">
+          <a
+            href="https://srv9z.codesandbox.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className="projectlink hwstext">History's Armory</h2>
+            <div className="flip">
+              <div className="front hws">
+                <h1 className="text-shadow skylimittitle"></h1>
+              </div>
+              <div className="back">
+                <p>
+                  History's Armory, a website for collectors and enthusiasts to
+                  buy historical reproduction armor and weapons. Made with React
+                  and Scss.
+                </p>
+                <div className="row iconrow">
+                  <div className="reacticon"></div>
+                  <div className="cssicon"></div>
+                  <div className="scssicon"></div>
+                </div>
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-</div>
-
-
-</div></div>
+      </div>
+    </div>
   </motion.div>
 );
