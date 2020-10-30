065b9faa73788b0b8dce8fb237d63f17e143d6b7
Dylan <dylanabozarth@gmail.com>
2020-10-30 11:14:02 -0700

    updated links

--- src/components/jymrdoodlesfocus.jsx
+++ src/components/jymrdoodlesfocus.jsx
@@ -37,7 +37,7 @@ export const JymrDoodlesFocus = ({ match }) => (
           
         </div>
       </motion.div>{" "}
-     <a href="https://56mvh.csb.app/" target="_blank" rel="noopener noreferrer"> <motion.img
+     <a href="https://1ecyq.csb.app/" target="_blank" rel="noopener noreferrer"> <motion.img
         variants={imageVariants}
         src="./images/jymr.jpg"
         alt="JYMR doodles"
@@ -47,7 +47,7 @@ export const JymrDoodlesFocus = ({ match }) => (
     <div className="linkbox">
       <a
         className="button2 b-blue rot-135"
-        href="https://rdxwy.csb.app/"
+        href="https://1ecyq.csb.app/"
         target="_blank"
         rel="noopener noreferrer"
       >
