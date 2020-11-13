6f5b8702af3b346e110337b805bf42c1cb4bcb1f
dylan <dylanabozarth@gmail.com>
2020-11-12 22:01:57 -0800

    fiex

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -1,7 +1,6 @@
 import React from "react";
 import TestCard from "../components/testcard";
 
-import { Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
 const changepage = {
   in: {
@@ -23,9 +22,9 @@ export const Testimonials = () => (
     transition={pagetransition}
     className="testimonialpage"
   >
-    <Container>
-      <Row>
-        <Col>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
           <TestCard
             imgUrl="https://skylimfitness.com/static/media/Sky.e0283525.jpg"
             title=" Skyler A. of Skylimit Fitness"
@@ -35,15 +34,15 @@ export const Testimonials = () => (
         
         I look forward to having a website that will result in more sales rather than drive clients away - I highly recommend Dylan for all your website needs."
           />
-        </Col>
-        <Col>
+        </div>
+        <div className="col-sm-6">
           <TestCard
             imgUrl="./images/brandon.jpg"
             title="Brandon B. Of Spokane Power Stroke"
             description="Dylan was an excellent communicator and dilligent worker throughout the making of my website. I would describe what I wanted and before I knew it he would have a preview ready for me to view. He's great at what he does. "
           />
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </div>
   </motion.div>
 );
