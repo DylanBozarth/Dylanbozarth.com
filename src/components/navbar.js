eaec4cd98da8bdf49157d6ee419046a9625ca627
dylan <dylanabozarth@gmail.com>
2020-11-16 15:42:49 -0800

    SOF question

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,34 +3,27 @@ import { Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 export const Navigation = () => (
-  <nav className="container navbarmaster">
-    <ul>
-      <li>
-        <NavLink to="/" className="nav-link">
-          {" "}
-          <Nav.Link href="#features">Home</Nav.Link>{" "}
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/Projects" className="nav-link">
-          {" "}
-          <Nav.Link href="#features">Projects</Nav.Link>{" "}
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/Skills" className="nav-link">
-          {" "}
-          <Nav.Link href="#features">Skills</Nav.Link>{" "}
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/Testimonials" className="nav-link">
-          {" "}
-          <Nav.Link href="#features">Testimonials</Nav.Link>{" "}
-        </NavLink>
-      </li>
-      
-      
-    </ul>
-  </nav>
+  <nav className="navbar bg-dark navbar-dark navbar-expand-sm">
+      <div className="container">
+        <a className="navbar-brand" href="#">Wisdom Pets</a>
+        <button
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#toggler-nav"
+          aria-controls="toggler-nav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon justify-content-end"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="toggler-nav">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="#">Home</a>
+            <a className="nav-item nav-link" href="#">Mission</a>
+            <a className="nav-item nav-link" href="#">Staff</a>
+            <a className="nav-item nav-link disabled" href="#">Testimonials</a>
+          </div>
+        </div>
+      </div>
+    </nav>
 );
