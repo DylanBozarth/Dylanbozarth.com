ded218df5b9e7a037d8cba83f55f83c60b95556c
dylan <dylanabozarth@gmail.com>
2021-10-06 21:55:18 -0700

    target aquired

--- src/components/games/jumpgame.js
+++ src/components/games/jumpgame.js
@@ -1,22 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import "./games.css"
+
 export const JumpGame = () => {
-    let player = document.getElementById('player')
-    function Jump() {
-        player.classlist.add('powerUp')
-        
-    }
-    document.addEventListener('keyup', event => {
+    const [playerPosition, setPlayerPosition] = useState({
+backgroundColor: 'green',
+height: '50px',
+width: '50px'
+    })
+    useEffect(() => {
+document.addEventListener('keyup', event => {
         if (event.code === 'Space') {
-         console.log(player)
+          test()
         }
       })
+    }, [])
+    function test() {
+console.log('aaa')
+    }
     return (
         <div className="masterDiv">
-        <div className='jumpgameBox'>
-Jumping jack
-<div id="player"></div>
-<div className="enemy"></div>
-        </div></div>
+<div className="jumpgameBox">
+    Try and survive lol
+
+    <div style={playerPosition}></div>
+</div>
+        </div>
     )
 }
