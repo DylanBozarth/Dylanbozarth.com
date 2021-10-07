5cddce5dfeab1d7a03cd7264298d10eacc8daff3
dylan <dylanabozarth@gmail.com>
2021-10-06 21:10:11 -0700

    element by id

--- src/components/games/jumpgame.js
+++ src/components/games/jumpgame.js
@@ -1,11 +1,21 @@
 import React, {useState} from 'react'
 import "./games.css"
 export const JumpGame = () => {
+    let player = document.getElementById('player')
+    function Jump() {
+        player.classlist.add('powerUp')
+        
+    }
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+         console.log(player)
+        }
+      })
     return (
         <div className="masterDiv">
         <div className='jumpgameBox'>
 Jumping jack
-<div className="player"></div>
+<div id="player"></div>
 <div className="enemy"></div>
         </div></div>
     )
