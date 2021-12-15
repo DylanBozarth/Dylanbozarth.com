d4be9f8391d743b62ba585ba4b1c06680dc9e997
imstupidpleasehelp <dylanabozarth@gmail.com>
2021-12-14 18:33:37 -0800

    homepage box bug

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,29 +5,28 @@ import Card from "../components/card";
 import { NavLink } from "react-router-dom";
 const changepage = {
   in: {
-    y: '0vh',
+    y: "0vh",
   },
   out: {
-    y: '100vh',
+    y: "100vh",
   },
 };
 const pagetransition = {
-  duration: 1.2
+  duration: 1.2,
 };
 const floatIn1 = {
   in: {
-    y:'0vh'
+    y: "0vh",
   },
   out: {
-    y: '100vh'
+    y: "100vh",
   },
-}
+};
 const floatTransition = {
-  duration: 1.2
-}
+  duration: 1.2,
+};
 
 function Homepage(props) {
- 
   return (
     <motion.div
       id=""
@@ -36,58 +35,43 @@ function Homepage(props) {
       exit="out"
       variants={changepage}
       transition={pagetransition}
-      className="homepagebox container"
+      className=" container-fluid homepage"
     >
-      
-     
-        <div className="row">
-          <motion.div initial="out"
-      animate="in"
-      exit="out"
-      
-       className="homepagelinkbox  col-lg-4" >
-            <NavLink to="/projects" onClick={() => props.setButtonOn(1)}>
-              <Card
-                imgUrl="./images/projectspage.png"
-                title="Professional Projects"
-                className="homepagecardimg "
-                description=""
-              />
-            </NavLink>
-          </motion.div>
-          <motion.div initial="out"
-      animate="in"
-      exit="out"
-      
-       className="homepagelinkbox  col-lg-4">
-            <NavLink to="/games"  onClick={() => props.setButtonOn(2)}>
-              <Card
-                imgUrl="./images/unprofessional.png"
-                title="Unprofessional Projects"
-                className="homepagecardimg "
-                description=""
-              />
-            </NavLink>
-          </motion.div>
-          <motion.div className="homepagelinkbox col-lg-4" 
-          initial="out"
-          animate="in"
-          exit="out"
+      <div className="row">
+        <motion.div initial="out" animate="in" exit="out" className=" col-sm-4">
+          <NavLink to="/projects" onClick={() => props.setButtonOn(1)}>
+            <Card
+              imgUrl="./images/projectspage.png"
+              title="Professional Projects"
+              className=" "
+              description=""
+            />
+          </NavLink>
+        </motion.div>
+        <motion.div initial="out" animate="in" exit="out" className="  col-sm-4">
+          <NavLink to="/games" onClick={() => props.setButtonOn(2)}>
+            <Card
+              imgUrl="./images/unprofessional.png"
+              title="Unprofessional Projects"
+              className=" "
+              description=""
+            />
+          </NavLink>
+        </motion.div>
+        <motion.div className=" col-sm-4" initial="out" animate="in" exit="out">
+          <a
+            href="https://drive.google.com/file/d/1uUpgsKgXbvMPbeHdmIUkTsQ7WLT6NpnW/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <a
-              href="https://drive.google.com/file/d/1uUpgsKgXbvMPbeHdmIUkTsQ7WLT6NpnW/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Card
-                imgUrl="./images/resume.png"
-                title="My Resume"
-                className="homepagecardimg"
-              />
-            </a>
-          </motion.div>
-          
-        </div>
+            <Card
+              imgUrl="./images/resume.png"
+              title="My Resume"
+              className=""
+            />
+          </a>
+        </motion.div>
+      </div>
     </motion.div>
   );
 }
