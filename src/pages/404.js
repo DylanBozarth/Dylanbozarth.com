26a625814ac29b2d98eadd0bf176b4a1b73f5fb2
Dylan <dylanabozarth@gmail.com>
2023-07-25 22:06:18 -0700

    remove old js

--- src/pages/404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-
-export const Fourohfour = () => {
-    return(
-        <div className="white text-center">Page not found, not sure how you got here. 
-        <br /> I recommend clicking one of the navigation links </div>
-    )
-}
